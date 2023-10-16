2cb3e8633105b9a9a521aad5dc92cfef1d37bfb7
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-10-16 04:03:51 -0500

    Make adjustments to the alert

--- js/site.js
+++ js/site.js
@@ -4,5 +4,5 @@ form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevents the default form submission behavior
 
   // Display a thank-you message using a browser alert
-  alert('Thank you for joining Reach New Heights: Mountain Climbing Club! You are now part of our mountain-climbing community. Get ready to conquer peaks and embrace the adventure.');
+  alert('Thank you for joining Reach New Heights: Mountain Climbing Club! We look forward to meeting you!');
 });
