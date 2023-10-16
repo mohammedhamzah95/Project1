84f2c40d86237ca2753f29e75b5c4c64d23f5c98
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-10-16 03:58:14 -0500

    Try browser alert

--- js/site.js
+++ js/site.js
@@ -3,11 +3,6 @@ const form = document.getElementById('signupForm');
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevents the default form submission behavior
 
-  const thankYouMessage = document.getElementById('thankYouMessage');
-  
-  // Check if the thank-you message is already displayed
-  if (!thankYouMessage.classList.contains('visible')) {
-    // Display the thank-you message
-    thankYouMessage.classList.add('visible');
-  }
+  // Display a thank-you message using a browser alert
+  alert('Thank you for joining Reach New Heights: Mountain Climbing Club! You are now part of our mountain-climbing community. Get ready to conquer peaks and embrace the adventure.');
 });
