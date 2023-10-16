a955e475c48f003f5653bc93f16a5dddb7cb76a5
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-10-16 01:43:13 -0500

    Disable repeat of thank you message

--- js/site.js
+++ js/site.js
@@ -1,15 +1,21 @@
 const form = document.getElementById('signupForm');
+let thankYouMessageDisplayed = false;
 
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevents the default form submission behavior
 
-  // Create and display the thank-you message with a smooth fade-in effect
-  const thankYouMessage = document.createElement('div');
-  thankYouMessage.innerHTML = `
-    <h2>Thank You for Joining Reach New Heights: Mountain Climbing Club!</h2>
-    <p>You are now part of our mountain-climbing community. Get ready to conquer peaks and embrace the adventure!</p>
-  `;
-  thankYouMessage.classList.add('thankYouMessage');
-  form.parentNode.appendChild(thankYouMessage);
-  thankYouMessage.classList.add('visible');
+  // Check if the thank-you message is already displayed
+  if (!thankYouMessageDisplayed) {
+    // Create and display the thank-you message with a smooth fade-in effect
+    const thankYouMessage = document.createElement('div');
+    thankYouMessage.innerHTML = `
+      <h2>Thank You for Joining Reach New Heights: Mountain Climbing Club!</h2>
+      <p>You are now part of our mountain-climbing community. Get ready to conquer peaks and embrace the adventure!</p>
+    `;
+    thankYouMessage.classList.add('thankYouMessage');
+    form.parentNode.appendChild(thankYouMessage);
+    thankYouMessage.classList.add('visible');
+    
+    thankYouMessageDisplayed = true;
+  }
 });
