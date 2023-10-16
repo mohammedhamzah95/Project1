ec2779ef59bdf28b07b166b24e66eaed63238d9d
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-10-16 02:15:41 -0500

    JS update

--- js/site.js
+++ js/site.js
@@ -1,20 +1,13 @@
 const form = document.getElementById('signupForm');
-const signupSection = document.getElementById('signupSection');
-let thankYouMessageDisplayed = false;
 
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevents the default form submission behavior
 
+  const thankYouMessage = document.getElementById('thankYouMessage');
+  
   // Check if the thank-you message is already displayed
-  if (!thankYouMessageDisplayed) {
-    // Create and display the thank-you message with a smooth fade-in effect
-    const thankYouMessage = document.createElement('div');
-    thankYouMessage.innerHTML = `
-      <h2>Thank You for Joining Reach New Heights: Mountain Climbing Club!</h2>
-      <p>You are now part of our mountain-climbing community. Get ready to conquer peaks and embrace the adventure!</p>
-    `;
-    thankYouMessage.classList.add('thankYouMessage');
-    form.parentNode.appendChild(thankYouMessage);
+  if (!thankYouMessage.classList.contains('visible')) {
+    // Display the thank-you message
     thankYouMessage.classList.add('visible');
-
-    thankYouMessageDisplayed = true;
+  }
+});
