a352394b515d6b654fe4213ed65ad37354b81e4f
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-11-04 04:26:34 -0500

    Put prevent default for field validation in JS

--- js/site.js
+++ js/site.js
@@ -2,10 +2,9 @@ const form = document.getElementById('signupForm');
 const fullNameInput = document.getElementById('fullName');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevents the default form submission behavior
-
   // Field validation
   if (fullNameInput.value.trim() === '') {
+    event.preventDefault(); // Prevents the default form submission behavior only if the full name field is empty
     alert('Please enter your full name.'); // Display an alert if the full name field is empty
   } else {
     alert('Thank you for joining Reach New Heights: Mountain Climbing Club! We look forward to meeting you!');
