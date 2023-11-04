d59ac4e2d90e0b6d3649d52b06c0b549428cf33b
mohammedhamzah95 <mohammedhamzah1995@gmail.com>
2023-11-04 03:52:46 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -4,10 +4,7 @@ const fullNameInput = document.getElementById('fullName');
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevents the default form submission behavior
 
-  // Display a thank-you message using a browser alert
-  alert('Thank you for joining Reach New Heights: Mountain Climbing Club! We look forward to meeting you!');
-});
-// Field validation
+  // Field validation
   if (fullNameInput.value.trim() === '') {
     alert('Please enter your full name.'); // Display an alert if the full name field is empty
   } else {
